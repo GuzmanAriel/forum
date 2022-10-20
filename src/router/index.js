3a18865ad180a1bb592a985ae12de37b6186b0af
GuzmanAriel <guzmanariel525@gmail.com>
2022-10-20 15:18:59 -0500

    Added the thread list and list router

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import PageHome from '@/components/PageHome'
+import { createRouter, createWebHistory } from 'vue-router'
+import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
+import sourceData from '@/data.json'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true,
+    beforeRouteEnter (to, from, next) {
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if thread exists
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
